24e0a1298473f9ce7d9607107f04d2c4015c3d25
Jerred K. Schafer <jkschafer@albany.edu>
2021-04-06 11:22:20 -0400

    fix as.mcmc issue

--- R/Functions/PhyloCorr.R
+++ R/Functions/PhyloCorr.R
@@ -2,8 +2,8 @@ library(MCMCglmm)
 
 # Function of getting phylogenetic correlation, HPDinterval, plot
 phylo_corr <- function(covar_XY, var_X, var_Y) {
-  post.mode <- posterior.mode(covar_XY/sqrt(var_X * var_Y))
-  post.hpd <- HPDinterval(covar_XY/sqrt(var_X * var_Y))
-  post.plot <- plot(covar_XY/sqrt(var_X * var_Y))
+  post.mode <- posterior.mode(as.mcmc(covar_XY/sqrt(var_X * var_Y)))
+  post.hpd <- HPDinterval(as.mcmc(covar_XY/sqrt(var_X * var_Y)))
+  post.plot <- plot(as.mcmc(covar_XY/sqrt(var_X * var_Y)))
   return(list(post.mode, post.hpd, post.plot))
 }
