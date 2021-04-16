597b4759f7488ccc015a165cb4d2a257d437b4bd
Jerred K. Schafer <jkschafer@albany.edu>
2021-04-16 19:07:40 -0400

    change plots

--- R/phylo_plots.R
+++ R/phylo_plots.R
@@ -85,31 +85,72 @@ plot(vtd_cont_map,
      xlim = c(0, 53))
 
 #------------- SSD and VTD ---------------#
+is_tip <- tree$edge[,2] <= length(tree$tip.label)
+ordered_tips <- tree$edge[is_tip, 2]
+
 layout(matrix(1:3, 1, 3),
-       widths=c(0.39, 0.22, 0.39))
+       widths=c(0.39, 0.2, 0.39))
 plot(vtd_cont_map,
      lwd = 6,
      outline = TRUE,
-     fsize=c(0, 1.5),
+     fsize=c(0, 1.2),
      ftype = "off",
      xlim = c(0, 53),
      leg.txt = "log(VTD + 1)")
 plot.new()
-plot.window(xlim = c(-0.5, 0.5),
+plot.window(xlim = c(-0.1, 0.1),
             ylim = get("last_plot.phylo",
                        envir = .PlotPhyloEnv)$y.lim)
 par(cex = 0.6)
-text(rep(0, length(vtd_cont_map$tree$tip.label)),
-     1:Ntip(vtd_cont_map$tree),
+text(rep(0, length(tree$tip.label[ordered_tips])),
+     1:Ntip(tree),
      gsub("_", " ",
-          vtd_cont_map$tree$tip.label), font = 3)
+          rev(tree$tip.label[ordered_tips])), font = 3)
 plot(ssd_cont_map,
      lwd = 6,
      outline = TRUE,
-     fsize = c(0, 1.5),
+     fsize = c(0, 1.2),
      xlim = c(0, 53),
      direction = "leftwards",
      ftype = "off",
      leg.txt = "log(SSD)")
 
-#---------------- OVsigns and VTD --------------------#
+
+#---------- Plot of ovulation signals on tree ---------------#
+rownames(reduced_data) <- reduced_data$Species
+
+ovsig <- setNames(reduced_data$Ovulation_Signs, 
+                  rownames(reduced_data))
+
+cols <- setNames(c("#F0E442", "#0072B2", 
+                   "#D55E00", "#CC79A7"),
+                 unique(as.vector(sapply(ovsig, levels))))
+cols
+
+mtrees <- make.simmap(tree, 
+                      ovsig, 
+                      model = "ER", 
+                      nsim = 100)
+
+
+densityTree(mtrees, method = "plotSimmap",
+            lwd = c(8), nodes = "intermediate",
+            colors = cols, ylim = c(0, 99),
+            compute.consensus = FALSE,
+            use.gradient = FALSE,
+            use.edge.length = T,
+            fsize = 0.5,
+            ftype = "i",
+            show.axis = F)
+legend("bottomleft",
+       legend = c("absent",
+                  "absent/slight",
+                  "slight",
+                  "present"),
+       col = cols,
+       cex = 1,
+       pch = 19)
+
+
+
+
