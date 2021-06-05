7cbd0e6f7dc9ccec589a1454dc78cd081aa8ded4
Jerred K. Schafer <jkschafer@albany.edu>
2021-06-04 22:36:49 -0400

    clean up code

--- R/data_cleaning.R
+++ R/data_cleaning.R
@@ -1,11 +1,14 @@
 library(tidyverse)
 library(ape)
 
-mainData <- read.csv("./Data/Grueter_et_al_2015_Rooker&Gavrilets2018_combined_data.csv",
-                     stringsAsFactors = FALSE)
+datfile <- "./Data/Grueter_et_al_2015_Rooker&Gavrilets2018_combined_data.csv"
+mainData <- read.csv(datfile, stringsAsFactors = FALSE)
 
-names(mainData)[names(mainData) == "Visual.trait.dimorphism.score.without..sexual.dichromatism"] <- "VTDwoSD"
-names(mainData)[names(mainData) == "Visual.trait.dimorphism.score.value.with.sexual.dichromatism"] <- "VTDwSD"
+vtd_longname <- "Visual.trait.dimorphism.score.without..sexual.dichromatism"
+names(mainData)[names(mainData) == 
+                  vtd_longname] <- "VTDwoSD"
+longname2 <- "Visual.trait.dimorphism.score.value.with.sexual.dichromatism"
+names(mainData)[names(mainData) == longname2] <- "VTDwSD"
 mainData$Species <- as.factor(mainData$Species)
 mainData$Habitat <- as.factor(mainData$Habitat)
 mainData$Social.organization[mainData$Social.organization == "unk"] <- NA
@@ -14,21 +17,26 @@ mainData$Social.organization <- as.factor(mainData$Social.organization)
 mainData$Fission.fusion <- as.factor(mainData$Fission.fusion)
 mainData$Ovulation_Signs[mainData$Ovulation_Signs == ""] <- NA
 
-# round VTD scores so that they are integers -> can use poisson distribution
+# round VTD scores so that they are integers -> 
+# can use poisson distribution
 mainData$VTDwSD_roundedUP <- ceiling(mainData$VTDwSD)
 mainData$VTDwoSD_roundedUP <- ceiling(mainData$VTDwoSD)
 mainData$VTDwSD_roundedDN <- floor(mainData$VTDwSD)
 mainData$VTDwoSD_roundedDN <- floor(mainData$VTDwoSD)
 
-# Creating binary variable for ovulation signals -> absent, slight, absent_slight = 0; present = 1
-mainData$Ovulation_Signs_bin <- ifelse(mainData$Ovulation_Signs == "Present",
+# Creating binary variable for ovulation signals -> absent, 
+# slight, absent_slight = 0; present = 1
+mainData$Ovulation_Signs_bin <- ifelse(mainData$Ovulation_Signs == 
+                                         "Present",
                                        "1", "0")
 mainData$Ovulation_Signs_bin <- as.factor(mainData$Ovulation_Signs_bin)
 # Creating dummy variables for ovulation signals categories
-mainData$Ovulation_Signs <- ifelse(mainData$Ovulation_Signs == "Absent", 
+mainData$Ovulation_Signs <- ifelse(mainData$Ovulation_Signs == 
+                                   "Absent", 
                                    "0", 
                                    mainData$Ovulation_Signs)
-mainData$Ovulation_Signs <- ifelse(mainData$Ovulation_Signs == "Absent_Slight", 
+mainData$Ovulation_Signs <- ifelse(mainData$Ovulation_Signs == 
+                                     "Absent_Slight", 
                                    "1", 
                                    mainData$Ovulation_Signs)
 mainData$Ovulation_Signs <- ifelse(mainData$Ovulation_Signs == "Slight", 
@@ -43,14 +51,18 @@ mainData$Ovulation_Signs <- as.factor(mainData$Ovulation_Signs)
 mainData$Species <- gsub(" ", "_", mainData$Species)
 
 names(mainData)[names(mainData) == "Body.mass.dimorphism..g."] <- "SSD"
-reduced_data <- mainData[complete.cases(mainData[,c("Habitat", "Social.organization",
-                                                    "Group.size", "Ovulation_Signs",
-                                                    "VTDwSD", "SSD")]),]
+reduced_data <- mainData[complete.cases(mainData[,c("Habitat", 
+                                                    "Social.organization",
+                                                    "Group.size", 
+                                                    "Ovulation_Signs",
+                                                    "VTDwSD", 
+                                                    "SSD")]),]
 
 #------------------ Phylogenetic tree -----------------#
 tenKtree <- read.nexus("./Data/consensusTree_10kTrees_Primates_Version3.nex")
 
-not_in_tree_reduced <- reduced_data[!reduced_data$Species %in% tenKtree$tip.label,]
+not_in_tree_reduced <- reduced_data[!reduced_data$Species %in% 
+                                      tenKtree$tip.label,]
 unique(not_in_tree_reduced$Species) # many
 
 reduced_data$Species <- gsub("Gorilla_gorilla_", 
@@ -82,7 +94,8 @@ reduced_data$Species <- gsub("Mico_argentatus",
                              reduced_data$Species)
 
 # Run it again
-not_in_tree_reduced <- reduced_data[!reduced_data$Species %in% tenKtree$tip.label,]
+not_in_tree_reduced <- reduced_data[!reduced_data$Species %in% 
+                                      tenKtree$tip.label, ]
 unique(not_in_tree_reduced$Species) # NONE!
 
 tree <- drop.tip(tenKtree, 
